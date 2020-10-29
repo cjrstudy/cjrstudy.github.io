cdb96eec52c55a577c3724a885b32813670b43d5
cjrstudy <72115206+cjrstudy@users.noreply.github.com>
2020-10-29 17:23:30 +0800

    Add files via upload

---
+++ ds/例题/3-test.c
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#define MaxStackSize 100
+typedef int DataType;
+#include "SeqStack.h"
+
+void main()
+{
+	SeqStack myStack;
+	int i, x;
+	StackInitiate(&myStack);
+	for(i=0; i<10; i++)
+	{
+		StackPush(&myStack, i+1);
+	}
+	StackTop(myStack, &x);
+	printf("当前栈顶元素为：%d\n", x);
+	printf("依次出栈的数据元素序列如下：\n");
+	while(StackNotEmpty(myStack))
+	{
+		StackPop(&myStack, &x);
+		printf("%d  ", x);
+	}
+}
