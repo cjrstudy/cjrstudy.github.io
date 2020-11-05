006e5c6939518141c978c8af2563f0c9cf31f24d
cjrstudy <72115206+cjrstudy@users.noreply.github.com>
2020-11-05 16:47:58 +0800

    Create 3-4.c

---
+++ ds/例题/3-4.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <ctype.h>
+typedef int DataType;
+#include "LinStack.h"
+int PostExp(char str[])
+{
+	DataType x, x1, x2;
+	int i;
+	LSNode *head;
+	StackInitiate(&head);
+	for(i=0; str[i] != '#'; i++)
+	{
+		if(isdigit(str[i]))
+		{
+			x = (int)(str[i] - 48);
+			StackPush(head, x);
+		}
+		else
+		{
+			StackPop(head, &x2);
+			StackPop(head, &x1);
+			switch(str[i])
+			{
+				case '+': {x1 += x2; break;}
+				case '-': {x1 -= x2; break;}
+				case '*': {x1 *= x2; break;}
+				case '/': 
+					if(x2 == 0)
+					{
+						printf("除数为0错！\n");
+						exit(0);
+					}
+					else
+					{
+						x1 /= x2;
+						break;
+					}
+			}
+			StackPush(head, x1);
+		}
+	}
+	StackPop(head, &x);
+	return x;
+}
+
+void main()
+{
+	char str[] = "3642/-5*+#";
+	int result;
+	result = PostExp(str);
+	printf("后缀算术表达式计算结果为：%d", result);
+}
+
+
+
+
